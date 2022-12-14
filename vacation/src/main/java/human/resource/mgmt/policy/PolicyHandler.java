c2905ad01ab06b44c9fd53bb8c2030d9efcd2c89
Jinyoung Jang <jyjang@uengine.org>
2022-12-14 09:49:39 +0900

    commit

---
+++ vacation/src/main/java/human/resource/mgmt/policy/PolicyHandler.java
@@ -0,0 +1,68 @@
+package human.resource.mgmt.policy;
+
+import human.resource.mgmt.aggregate.*;
+import human.resource.mgmt.command.*;
+import human.resource.mgmt.event.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.config.ProcessingGroup;
+import org.axonframework.eventhandling.DisallowReplay;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@ProcessingGroup("vacation")
+public class PolicyHandler {
+
+    @Autowired
+    CommandGateway commandGateway;
+
+    @EventHandler
+    //@DisallowReplay
+    public void wheneverVacationRegistered_Use(
+        VacationRegisteredEvent vacationRegistered
+    ) {
+        System.out.println(vacationRegistered.toString());
+
+        UseCommand command = new UseCommand();
+        commandGateway.send(command);
+    }
+
+    @EventHandler
+    //@DisallowReplay
+    public void wheneverVacationCancelled_Add(
+        VacationCancelledEvent vacationCancelled
+    ) {
+        System.out.println(vacationCancelled.toString());
+
+        AddCommand command = new AddCommand();
+        commandGateway.send(command);
+    }
+
+    @EventHandler
+    //@DisallowReplay
+    public void wheneverVacationRejected_Add(
+        VacationRejectedEvent vacationRejected
+    ) {
+        System.out.println(vacationRejected.toString());
+
+        AddCommand command = new AddCommand();
+        commandGateway.send(command);
+    }
+
+    @EventHandler
+    //@DisallowReplay
+    public void wheneverEmployeeJoined_RegisterUser(
+        EmployeeJoinedEvent employeeJoined
+    ) {
+        System.out.println(employeeJoined.toString());
+
+        RegisterUserCommand command = new RegisterUserCommand();
+        commandGateway.send(command);
+    }
+}
